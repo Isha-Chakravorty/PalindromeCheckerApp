abf417c97ede75c4d4cfd9f0b9f4d1f32ebf5059
Isha-Chakravorty <ishachakravorty857@gmail.com>
2026-03-05 08:53:21 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,102 +1,8 @@
 import java.util.*;
 
-public class PalindromeCheckerApp {
+public class UseCase13PalindromeCheckerApp {
 
-    public static void main(String[] args) {
-
-        // UC1: Welcome Message
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.\n");
-
-        // UC2: Hardcoded palindrome
-        uc2_HardcodedPalindrome();
-
-        // UC3: Reverse string method
-        uc3_StringReverse();
-
-        // UC4: Character array method
-        uc4_CharArrayMethod();
-
-        // UC5: Stack method
-        uc5_StackMethod();
-
-        // UC6: Queue + Stack method
-        uc6_QueueStackMethod();
-
-        // UC7: Deque method
-        uc7_DequeMethod();
-    }
-
-    // UC2
-    public static void uc2_HardcodedPalindrome() {
-
-        String input = "madam";
-        boolean isPalindrome = true;
-
-        for (int i = 0; i < input.length() / 2; i++) {
-
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("UC2 Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome + "\n");
-    }
-
-
-    // UC3
-    public static void uc3_StringReverse() {
-
-        String input = "level";
-        String reversed = "";
-
-        for (int i = input.length() - 1; i >= 0; i--) {
-
-            reversed += input.charAt(i);
-        }
-
-        boolean isPalindrome = input.equals(reversed);
-
-        System.out.println("UC3 Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome + "\n");
-    }
-
-
-    // UC4
-    public static void uc4_CharArrayMethod() {
-
-        String input = "radar";
-
-        char[] chars = input.toCharArray();
-
-        int start = 0;
-        int end = chars.length - 1;
-
-        boolean isPalindrome = true;
-
-        while (start < end) {
-
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-
-            start++;
-            end--;
-        }
-
-        System.out.println("UC4 Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome + "\n");
-    }
-
-
-    // UC5
-    public static void uc5_StackMethod() {
-
-        String input = "noon";
+    public static boolean stackMethod(String input) {
 
         Stack<Character> stack = new Stack<>();
 
@@ -104,54 +10,16 @@ public class PalindromeCheckerApp {
             stack.push(c);
         }
 
-        boolean isPalindrome = true;
-
         for (char c : input.toCharArray()) {
-
             if (c != stack.pop()) {
-                isPalindrome = false;
-                break;
+                return false;
             }
         }
 
-        System.out.println("UC5 Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome + "\n");
+        return true;
     }
 
-
-    // UC6
-    public static void uc6_QueueStackMethod() {
-
-        String input = "civic";
-
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-
-            queue.add(c);
-            stack.push(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while (!queue.isEmpty()) {
-
-            if (!queue.remove().equals(stack.pop())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("UC6 Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome + "\n");
-    }
-
-
-    // UC7
-    public static void uc7_DequeMethod() {
-
-        String input = "refer";
+    public static boolean dequeMethod(String input) {
 
         Deque<Character> deque = new ArrayDeque<>();
 
@@ -159,17 +27,62 @@ public class PalindromeCheckerApp {
             deque.add(c);
         }
 
-        boolean isPalindrome = true;
-
         while (deque.size() > 1) {
 
             if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
+                return false;
             }
         }
 
-        System.out.println("UC7 Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome + "\n");
+        return true;
+    }
+
+    public static boolean twoPointerMethod(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "racecar";
+
+        long startTime1 = System.nanoTime();
+        boolean result1 = stackMethod(input);
+        long endTime1 = System.nanoTime();
+        long stackTime = endTime1 - startTime1;
+
+        long startTime2 = System.nanoTime();
+        boolean result2 = dequeMethod(input);
+        long endTime2 = System.nanoTime();
+        long dequeTime = endTime2 - startTime2;
+
+        long startTime3 = System.nanoTime();
+        boolean result3 = twoPointerMethod(input);
+        long endTime3 = System.nanoTime();
+        long twoPointerTime = endTime3 - startTime3;
+
+        System.out.println("Input : " + input + "\n");
+
+        System.out.println("Stack Method Result : " + result1);
+        System.out.println("Execution Time : " + stackTime + " ns\n");
+
+        System.out.println("Deque Method Result : " + result2);
+        System.out.println("Execution Time : " + dequeTime + " ns\n");
+
+        System.out.println("Two Pointer Method Result : " + result3);
+        System.out.println("Execution Time : " + twoPointerTime + " ns");
     }
 }
